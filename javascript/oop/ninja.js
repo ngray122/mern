478324eed30699a9efe77b01430a5c6d9bd49db2
Nicole Gray <ngray122@gmail.com>
2022-01-10 19:41:44 -0500

    added ninja instance

--- javascript/oop/ninja.js
+++ javascript/oop/ninja.js
@@ -1,13 +1,10 @@
 class Ninja {
-    constructor() {
+    constructor(name, health, speed, strength ) {
         this.name = name
         this.health = health
         this.speed = 3
         this.strength = 3
     }
-
-
-
 
     sayName() {
         console.log(`My name is ${this.name}`)
@@ -19,8 +16,17 @@ class Ninja {
 
     drinkSake() {
         this.health += 10 
-        return this.health
+        console.log(`Your health has been increased by 10 and is now ${this.health} `)
 
     }
 
 }
+
+const ninja1 = new Ninja("Jackie", 7);
+console.log(ninja1)
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.drinkSake();
+
+
