1d9bcdf9019886ce769d394f212c9ed3e36c3bcd
Nicole Gray <ngray122@gmail.com>
2022-01-11 15:13:55 -0500

    It's time to duel

--- javascript/oop/time_to_duel.js
+++ javascript/oop/time_to_duel.js
@@ -7,16 +7,19 @@ class Card {
 
 
 class Unit extends Card {
-    constructor(name, cost, power, res) {
+    constructor(name, cost, power, resiliance) {
         super(name, cost);
         this.power = power;
-        this.res = res;
+        this.resiliance = resiliance;
 
     }
 
     attack(target) {
-        target.res -= power
-
+        if (target instanceof Unit) {
+            target.resiliance -= target.power
+        } else {
+            throw new Error("Target must be a unit!");
+        }
     }
 }
 
@@ -28,13 +31,44 @@ class Effect extends Card {
         this.stat = stat;
         this.magnitude = magnitude
     }
+
+
+
+    play(target) {
+        if (target instanceof Unit) {
+            target[this.stat] += this.magnitude
+            // if(this.stat == "power"){
+            //     target.power += this.magnitude
+                
+            // } else if (this.stat == "resiliance") {
+            //     target.resiliance += this.magnitude
+            // }
+        } else {
+            throw new Error("Target must be a unit!");
+        }
+    }
 }
 
 const redBeltNinja = new Unit("Red Belt Ninja", 3, 3, 4) 
 const blackBeltNinja = new Unit("Black Belt Ninja", 4, 5, 4) 
+const hardAlgo = new Effect("Hard Algorithm", 2, "Increase target's resilience by 3", "resiliance", 3);
+const promiseRejection = new Effect("Unhandled Promise Rejection", 1, "Reduce target's resilience by 2", "resiliance", -2);
+const pairProgramming = new Effect("Pair Programming", 3, "Increase target's power by 2", "power", 2);
 
 
-const hardAlgo = new Effect("Hard Algorithm", 2, "Increase target's resilience by 3", "resilience", +3)
-const promiseRejection = new Effect("Unhandled Promise Rejection", 1, "Reduce target's resilience by 2", "resilience", -2)
-const pairProgramming = new Effect("Pair Programming", 3, "Increase target's power by 2", "power", +2)
+hardAlgo.play(redBeltNinja);
+console.log(redBeltNinja)
+hardAlgo.play(redBeltNinja);
+console.log(redBeltNinja)
 
+promiseRejection.play(redBeltNinja)
+console.log(redBeltNinja)
+
+pairProgramming.play(redBeltNinja)
+console.log(redBeltNinja)
+
+
+redBeltNinja.attack(blackBeltNinja);
+console.log(blackBeltNinja)
+blackBeltNinja.attack(redBeltNinja);
+console.log(redBeltNinja)
