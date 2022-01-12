dd78b5d3718a142f0425900307e5bb3ef78126d4
Nicole Gray <ngray122@gmail.com>
2022-01-12 15:38:35 -0500

    formatting

--- react/prop-it-up/src/App.jsx
+++ react/prop-it-up/src/App.jsx
@@ -6,16 +6,24 @@ function App() {
   return (
     <div className="App">
       <div>
-        <PersonCard firstName={"Nicole"} lastName={"Gray"} age={35} hairColor={'Light Brown'}></PersonCard>
+        <PersonCard firstName={"Nicole"} lastName={"Gray"} age={35} hairColor={'Light Brown'}>
+
+        </PersonCard>
       </div>
       <div>
-        <PersonCard firstName={"Bob"} lastName={"Belcher"} age={48} hairColor={'Black'}></PersonCard>
+        <PersonCard firstName={"Bob"} lastName={"Belcher"} age={48} hairColor={'Black'}>
+
+        </PersonCard>
       </div>
       <div>
-        <PersonCard firstName={"Homer"} lastName={"Simpson"} age={57} hairColor={'NA'}></PersonCard>
+        <PersonCard firstName={"Homer"} lastName={"Simpson"} age={57} hairColor={'Bald'}>
+
+        </PersonCard>
       </div>
       <div>
-        <PersonCard firstName={"Tj"} lastName={"Osborne"} age={25} hairColor={'Brown'}></PersonCard>
+        <PersonCard firstName={"Tj"} lastName={"Osborne"} age={25} hairColor={'Brown'}>
+
+        </PersonCard>
       </div>
     </div>
   );
