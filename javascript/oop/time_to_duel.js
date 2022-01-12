6a5d7a64bb1e27142e856c04f344f2925f57ca08
Nicole Gray <ngray122@gmail.com>
2022-01-11 20:04:56 -0500

    first react

--- javascript/oop/time_to_duel.js
+++ javascript/oop/time_to_duel.js
@@ -51,13 +51,15 @@ class Effect extends Card {
 
 const redBeltNinja = new Unit("Red Belt Ninja", 3, 3, 4) 
 const blackBeltNinja = new Unit("Black Belt Ninja", 4, 5, 4) 
+
+
+
 const hardAlgo = new Effect("Hard Algorithm", 2, "Increase target's resilience by 3", "resiliance", 3);
 const promiseRejection = new Effect("Unhandled Promise Rejection", 1, "Reduce target's resilience by 2", "resiliance", -2);
 const pairProgramming = new Effect("Pair Programming", 3, "Increase target's power by 2", "power", 2);
 
 
-hardAlgo.play(redBeltNinja);
-console.log(redBeltNinja)
+
 hardAlgo.play(redBeltNinja);
 console.log(redBeltNinja)
 
