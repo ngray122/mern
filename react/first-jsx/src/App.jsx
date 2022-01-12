e456a52759d4190def81cf34a682044b156f2be1
Nicole Gray <ngray122@gmail.com>
2022-01-11 21:59:49 -0500

    First JSX - need to fix bullet style

--- react/first-jsx/src/App.jsx
+++ react/first-jsx/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-// import dojo from './components/dojo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Paint my kitchen</li>
+        <li>Sky Dive over Grand Canyon</li>
+        <li>Sleep in a haunted house overnight</li>
+        <li>Study</li>
+      </ul>
+
+      
     </div>
   );
 }
