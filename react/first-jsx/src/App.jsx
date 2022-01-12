cb21d74826a9cc275080e25aeb93659b41a5549a
Nicole Gray <ngray122@gmail.com>
2022-01-11 21:48:11 -0500

    my first jsx - folder setup

---
+++ react/first-jsx/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import dojo from './components/dojo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default App;
