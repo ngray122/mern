a64775359439342f63786ea3d46a1b3c8630ae9b
Nicole Gray <ngray122@gmail.com>
2022-01-13 15:52:16 -0500

    hook form complete

--- react/hook-form/src/App.jsx
+++ react/hook-form/src/App.jsx
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App container">
+    <div className="App " >
       <User>
-        
+
       </User>
 
     </div>
--- react/hook-form/src/components/User.jsx
+++ react/hook-form/src/components/User.jsx
@@ -1,48 +1,61 @@
 import React, { useState } from 'react';
 
+const formStyle = {
+    width: '400px'
+};
+
 
 
 const User = props => {
-    
+
+    let [firstName, setFirstname] = useState('');
+    let [lastName, setLastname] = useState('');
+    let [email, setEmail] = useState('');
+    let [password, setPassword] = useState('');
+    let [confirmPassword, setConfirmPassword] = useState('');
+
 
     
     return(
-        <div>
+        <div >
+            <div style= {formStyle} className="container">
+            <h1>Regsitration</h1>
+        
             <form action="">
-                <div>
-                <label htmlFor="">First Name:</label>
-                <input type="text" />
+                <div className= "form-group">
+                <label htmlFor="" className="form-label">First Name:</label>
+                <input type="text" className="form-control" onChange={(e)=> setFirstname(e.target.value)}/>
                 </div>
 
-                <div>
-                <label htmlFor="">Last Name:</label>
-                <input type="text" />
+                <div className= "form-group">
+                <label htmlFor="" className="form-label">Last Name:</label>
+                <input type="text" className="form-control" onChange={(e)=> setLastname(e.target.value)}/>
                 </div>
 
-                <div>
-                <label htmlFor="">Age</label>
-                <input type="number" />
+                <div className= "form-group">
+                <label htmlFor="" className="form-label">Email</label>
+                <input type="email" className="form-control" onChange={(e)=> setEmail(e.target.value)}/>
                 </div>
 
-                <div>
-                <label htmlFor="">Password:</label>
-                <input type="password" />
+                <div className= "form-group">
+                <label htmlFor="" className="form-label">Password:</label>
+                <input type="password" className="form-control" onChange={(e)=> setConfirmPassword(e.target.value)}/>
                 </div>
 
-                <div>
-                <label htmlFor="">Confirm Password:</label>
-                <input type="password" />
+                <div className= "form-group">
+                <label htmlFor="" className="form-label">Confirm Password:</label>
+                <input type="password" className="form-control" onChange={(e)=> setEmail(e.target.value)}/>
                 </div>
-                
-                
-                
 
-
-
-
+                <input type="submit" />
             </form>
-
-
+            <hr />
+            <p>First Name: {firstName}</p>
+            <p>Last Name: {lastName}</p>
+            <p>Email: {email}</p>
+            <p>Password: {password}</p>
+            <p>Confirm Password: {confirmPassword}</p>
+            </div>
 
         </div>
     );
