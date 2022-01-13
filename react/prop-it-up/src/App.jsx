c9a5bd363e79f42a839abae360dac501e2bf2874
Nicole Gray <ngray122@gmail.com>
2022-01-12 19:25:31 -0500

    prop it up with boostrap

--- react/prop-it-up/src/App.jsx
+++ react/prop-it-up/src/App.jsx
@@ -1,29 +1,28 @@
 
-import './App.css';
 import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <PersonCard firstName={"Nicole"} lastName={"Gray"} age={35} hairColor={'Light Brown'}>
+      <div className="container">
+        <div>
+          <PersonCard firstName={"Nicole"} lastName={"Gray"} age={35} hairColor={'Light Brown'}>
+          </PersonCard>
+        </div>
+        <div>
+          <PersonCard firstName={"Bob"} lastName={"Belcher"} age={48} hairColor={'Black'}>
+          </PersonCard>
+        </div>
+        <div>
+          <PersonCard firstName={"Homer"} lastName={"Simpson"} age={57} hairColor={'Bald'}>
 
-        </PersonCard>
-      </div>
-      <div>
-        <PersonCard firstName={"Bob"} lastName={"Belcher"} age={48} hairColor={'Black'}>
+          </PersonCard>
+        </div>
+        <div>
+          <PersonCard firstName={"Tj"} lastName={"Osborne"} age={25} hairColor={'Brown'}>
 
-        </PersonCard>
-      </div>
-      <div>
-        <PersonCard firstName={"Homer"} lastName={"Simpson"} age={57} hairColor={'Bald'}>
-
-        </PersonCard>
-      </div>
-      <div>
-        <PersonCard firstName={"Tj"} lastName={"Osborne"} age={25} hairColor={'Brown'}>
-
-        </PersonCard>
+          </PersonCard>
+        </div>
       </div>
     </div>
   );
--- react/prop-it-up/src/components/PersonCard.jsx
+++ react/prop-it-up/src/components/PersonCard.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 
+const cardStyle = {
+    width: '300px'
+}
 
 
 class PersonCard extends Component {
     render() {
-        const {firstName, lastName, age, hairColor} = this.props
+        const { firstName, lastName, age, hairColor } = this.props
         return (
-            <div>
-                <h1>{lastName}, {firstName} </h1>
-                <p>Age: {age }</p>
-                <p>Hair Color:  {hairColor}</p>
+            <div className="card" style={ cardStyle}>
+                <div className = "card-body">
+                    <h1 className="card-title">{lastName}, {firstName} </h1>
+                    <p className="card-text">Age: {age}</p>
+                    <p className="card-text">Hair Color:  {hairColor}</p>
+                </div>
             </div>
-
         )
     }
-    
+
 }
 
 export default PersonCard;
