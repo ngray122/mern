98d1b84023d50a915a9bc15e078b0300a646bb8e
Nicole Gray <ngray122@gmail.com>
2022-01-13 19:01:39 -0500

    more forms

--- react/hook-form/src/components/User.jsx
+++ react/hook-form/src/components/User.jsx
@@ -15,46 +15,102 @@ const User = props => {
     let [confirmPassword, setConfirmPassword] = useState('');
 
 
-    
-    return(
+
+    return (
         <div >
-            <div style= {formStyle} className="container">
-            <h1>Regsitration</h1>
-        
-            <form action="">
-                <div className= "form-group">
-                <label htmlFor="" className="form-label">First Name:</label>
-                <input type="text" className="form-control" onChange={(e)=> setFirstname(e.target.value)}/>
-                </div>
+            <div style={formStyle} className="container">
+                <h1>Registration</h1>
 
-                <div className= "form-group">
-                <label htmlFor="" className="form-label">Last Name:</label>
-                <input type="text" className="form-control" onChange={(e)=> setLastname(e.target.value)}/>
-                </div>
+                <form action="">
+                    <div className="form-group">
+                        <label htmlFor="" className="form-label">First Name:</label>
+                        <input type="text" className="form-control" onChange={(e) => setFirstname(e.target.value)} />
 
-                <div className= "form-group">
-                <label htmlFor="" className="form-label">Email</label>
-                <input type="email" className="form-control" onChange={(e)=> setEmail(e.target.value)}/>
-                </div>
+                        {
+                            firstName.length < 2 && firstName.length > 0
+                                ?
+                                <p className="text-danger">First Name must be at least 2 characters, length is {firstName.length}!</p>
+                                :
+                                ""
+                        }
 
-                <div className= "form-group">
-                <label htmlFor="" className="form-label">Password:</label>
-                <input type="password" className="form-control" onChange={(e)=> setConfirmPassword(e.target.value)}/>
-                </div>
 
-                <div className= "form-group">
-                <label htmlFor="" className="form-label">Confirm Password:</label>
-                <input type="password" className="form-control" onChange={(e)=> setEmail(e.target.value)}/>
-                </div>
+                    </div>
 
-                <input type="submit" />
-            </form>
-            <hr />
-            <p>First Name: {firstName}</p>
-            <p>Last Name: {lastName}</p>
-            <p>Email: {email}</p>
-            <p>Password: {password}</p>
-            <p>Confirm Password: {confirmPassword}</p>
+                    <div className="form-group">
+                        <label htmlFor="" className="form-label">Last Name:</label>
+                        <input type="text" className="form-control" onChange={(e) => setLastname(e.target.value)} />
+
+                        {
+                            lastName.length < 2 && lastName.length > 0
+                                ?
+                                <p className="text-danger">Last Name must be at least 2 characters, length is {lastName.length}!</p>
+                                :
+                                ""
+                        }
+
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="email" className="form-control" onChange={(e) => setEmail(e.target.value)} />
+
+                        {
+                            email.length < 5 && email.length > 0
+                                ?
+                                <p className="text-danger">Email must be at least 5 characters, length is {email.length}!</p>
+                                :
+                                ""
+                        }
+
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="" className="form-label">Password:</label>
+                        <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} />
+                        {
+                            password.length < 8 && password.length > 0
+                                ?
+                                <p className="text-danger">Password must be at least 8 characters, length is {password.length}!</p>
+                                :
+                                ""
+                        }
+
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="" className="form-label">Confirm Password:</label>
+                        <input type="password" className="form-control" onChange={(e) => setConfirmPassword(e.target.value)} />
+
+                        {
+                            confirmPassword.length > 0 || confirmPassword !== password 
+                            ?
+                                <p className="text-danger">Passwords must match!</p> 
+                            : 
+                                ""
+                        }
+
+
+{/* const validatePassword = (e) => {
+
+if (confirmPassword.length < 8 && confirmPassword !== password) {
+                            setConfirmPassword(e)
+    return (<p className="text-danger">Passwords must match!</p>)
+} else if (confirmPassword < 8 && confirmPassword > 0) {
+    return (<p>"Hello"</p>)
+}
+} */}
+
+                    </div>
+
+                    <input type="submit" />
+                </form>
+                <hr />
+                <p>First Name: {firstName}</p>
+                <p>Last Name: {lastName}</p>
+                <p>Email: {email}</p>
+                <p>Password: {password}</p>
+                <p>Confirm Password: {confirmPassword}</p>
             </div>
 
         </div>
