3d17a2ddae616bdac8c48719c7dcad1daa9e291e
Nicole Gray <ngray122@gmail.com>
2022-01-13 21:20:39 -0500

    box-generator

---
+++ react/box-generator/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import UserForm from './components/UserForm';
+import Boxes from './components/Boxes';
+
+function App() {
+  return (
+    <div className="App">
+      <UserForm></UserForm>
+      <Boxes></Boxes>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ react/box-generator/src/components/Boxes.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const Boxes = props => {
+
+    return (
+        <div className="container">
+
+            <h1>Hello from boxes</h1>
+
+        </div>
+
+    )
+
+
+}
+
+
+
+
+export default Boxes;
