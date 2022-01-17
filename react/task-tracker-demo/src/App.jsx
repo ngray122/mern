a34fe4a5a2f1058011f73e5a61e8442ffe725a80
Nicole Gray <ngray122@gmail.com>
2022-01-16 20:15:41 -0500

    toggle add task

--- react/task-tracker-demo/src/App.jsx
+++ react/task-tracker-demo/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from 'react';
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
         {   id: 1,
@@ -24,16 +26,36 @@ function App() {
     ]
 )
 
+// add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 // how is this working?
 // shows tasks NOT selected by clicking delete?
+// Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task)=> task.id !== id))
   }
 
+
+  // IF task is toggled 
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task)=> task.id === id ? { ...task, reminder: !task.reminder} : task))
+  }
+
   return (
+
     <div className="container">
-      <Header></Header>
-      <Tasks tasks={tasks} onDelete={deleteTask}></Tasks>
+      <Header onAdd={()=> setShowAddTask(!showAddTask)}></Header>
+
+      {showAddTask && <AddTask onAdd={addTask}></AddTask>}
+
+      {/* if tasks arr has tasks, display them on the page ELSE display "there are no tasks to be displayed "*/}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}></Tasks> : 'No tasks to show'}
+
     </div>
   );
 }
--- react/task-tracker-demo/src/components/Header.jsx
+++ react/task-tracker-demo/src/components/Header.jsx
@@ -5,15 +5,12 @@ import Button from './Button'
 
 
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Header = ({title, onAdd}) => {
     
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color="green" text="Add" onClick={onClick}></Button>
+            <Button color="green" text="Add" onClick={onAdd}></Button>
             
         </header>
     )
